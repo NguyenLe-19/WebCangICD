e22249400ded7e543cbdb6b95ef971b9cf015d65
NguyenLe-19 <thaonguyen2004.2021@gmail.com>
2025-06-26 10:48:45 +0700

    Update : tạo folder fragment và cập nhật các thẻ tag fragment

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,34 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("Home")
-	public String getHome() {
+	public String getHome(Model model) {
+		 model.addAttribute("currentPage", "home");
 		return "index";
 	}
+	
+	@RequestMapping("legal-documents")
+	public String getLegalDoc(Model model) {
+		 model.addAttribute("currentPage", "legal-document");
+		return "legal-documents";
+	}
+	
+	@RequestMapping("About")
+	public String getAbout(Model model) {
+		 model.addAttribute("currentPage", "about");
+		return "about";
+	}
+	
+	@RequestMapping("Service")
+	public String getService(Model model) {
+		 model.addAttribute("currentPage", "service");
+		return "service";
+	}
+	
+	
 }
